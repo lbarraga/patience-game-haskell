5682687fb2b923b09b7bc123186da8cbe2101d7e
lbarraga <lukas.barragantorres@ugent.be>
2022-11-15 19:27:04 +0100

    testen

--- test/VoorbeeldTest.hs
+++ test/VoorbeeldTest.hs
@@ -1,17 +1,17 @@
 import Test.Hspec
 
-import VoorbeeldModule (hoi, hallo)
-
 main :: IO ()
 main = hspec $ do 
-    it "Returns correct string for hoi" $ do
-        hoi `shouldBe` "Hoi"
 
-    it "Returns correct string for hallo" $ do
-        hallo `shouldBe` "Hallo"
-    it "Returns correct string for hallo" $ do
-        hallo `shouldBe` "Hallo"
-    it "Returns correct string for hallo" $ do
-        hallo `shouldBe` "Hallo"
-    it "Returns correct string for hallo" $ do
-        hallo `shouldBe` "Hallo"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
+    it "Returns correct string for hoi" $ do
+        "Hoi" `shouldBe` "Hoi"
