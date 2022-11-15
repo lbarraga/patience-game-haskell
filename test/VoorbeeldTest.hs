46bd75c3591d056f0a4bc21b6e7437b8930d7efd
lbarraga <lukas.barragantorres@ugent.be>
2022-11-15 19:57:07 +0100

    testen zijn nu deftig en slagen allemaal

--- test/VoorbeeldTest.hs
+++ test/VoorbeeldTest.hs
@@ -1,17 +1,18 @@
 import Test.Hspec
 
+import Cards (allCardsShown, isVisible, placeholderCard)
+import PatienceLogica (initPile, aantalSpeelVeldKaarten, initGameField, initEndingStacks)
+
 main :: IO ()
 main = hspec $ do 
 
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
-    it "Returns correct string for hoi" $ do
-        "Hoi" `shouldBe` "Hoi"
+    it "Er zijn 52 kaarten" $ do
+        length allCardsShown `shouldBe` 52
+    it "Elke kaart is visible" $ do
+        all isVisible allCardsShown `shouldBe` True
+    it "InitGameField maakt weldegelijk `aantalSpeelVeldKaarten` aantal stapels" $ do
+        (sum . map length) initGameField `shouldBe` aantalSpeelVeldKaarten
+    it "De pile bestaat uit de overige kaarten" $ do
+        length initPile `shouldBe` 52 - aantalSpeelVeldKaarten
+    it "De endingstacks bevatten initieel enkel de placeholdercard" $ do
+        all (==[placeholderCard]) initEndingStacks `shouldBe` True
